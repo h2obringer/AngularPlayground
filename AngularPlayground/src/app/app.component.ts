fd2726db9c7d6319bce82011685c49a2dba71aa3
h2obringer <randal.obringer@gmail.com>
2023-08-20 10:36:12 -0600

    Simple variable replacements

--- AngularPlayground/src/app/app.component.ts
+++ AngularPlayground/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularPlayground';
+    title = 'AngularPlayground';
+    message = 'test a message on the AppComponent.'
 }
