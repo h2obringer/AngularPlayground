19faa4bcbab1ed1b25dd6043c45a61130475c86a
h2obringer <randal.obringer@gmail.com>
2023-08-21 17:55:43 -0600

    Component - Styles Example

--- AngularPlayground/src/app/notification/notification.component.ts
+++ AngularPlayground/src/app/notification/notification.component.ts
@@ -6,11 +6,25 @@ import { Component } from '@angular/core';
     //CON: if the template contains any errors, we will not get any compile-time errors.
     //CON: this mixes Typescript code with HTML code (NOT BEST PRACTICES)
     template: `
-<div>
-    <p>This website uses cookies to provide better user experience.
-</div>
+        <div class="notification-div">
+            <p>This website uses cookies to provide better user experience.
+        </div>
     `,
-    styleUrls: ['./notification.component.css']
+    styles: [
+        `
+            .notification-div{
+                margin: 10px 0px;
+                padding: 10px 20px; 
+                background-color: #FAD7A0;
+                text-align: center;
+            }
+        `,
+        `
+            p{
+                font-size: 14px;
+            }
+        `
+    ]
 })
 export class NotificationComponent {
 
