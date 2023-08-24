bbc8b579aeb6f1c52f683faba00dd6d97ec9894a
h2obringer <randal.obringer@gmail.com>
2023-08-24 17:31:17 -0600

    View Encapsulation Example

--- AngularPlayground/src/app/footer/footer.component.ts
+++ AngularPlayground/src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css']
+    styleUrls: ['./footer.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
     constructor() { }
