bf9441f22ed12bb496d030ba2045b8e8f819f043
h2obringer <randal.obringer@gmail.com>
2023-08-24 15:52:16 -0600

    Template Reference Variable - Simple Example

--- AngularPlayground/src/app/footer/footer.component.ts
+++ AngularPlayground/src/app/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+    constructor() { }
+    ngOnInit(): void { }
 
+    sayHello(inputEl: HTMLInputElement) {
+        alert("Hello " + inputEl.value);
+    }
 }
