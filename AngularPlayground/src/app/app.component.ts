613d14f980607a1a1441c41fca7e44b356a7254f
h2obringer <randal.obringer@gmail.com>
2023-08-30 09:23:43 -0600

    More Observable Examples - 3 other methods

--- AngularPlayground/src/app/app.component.ts
+++ AngularPlayground/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
     title = 'AngularPlayground';
     message = 'test a message on the AppComponent.'
 
+    //method 1 - user Observable constructor
     myObservable = new Observable((observer) => {
         console.log('Observable starts');
         observer.next("1"); //emit this chunk of data
@@ -27,6 +28,28 @@ export class AppComponent implements OnInit {
         setTimeout(() => { observer.complete() }, 6000); //send the completion status
     });
 
+    //method 2 of creating an observable - similar to using the constructor...
+    //deprecated, will be removed soon...
+    myObservable2 = Observable.create((observer) => {
+        console.log('Observable starts');
+        observer.next("A"); //emit this chunk of data
+        setTimeout(() => { observer.next("B") }, 2000); //emit this chunk of data later
+        setTimeout(() => { observer.next("C") }, 3000); //emit this chunk of data later
+        setTimeout(() => { observer.next("D") }, 4000); //emit this chunk of data later
+        setTimeout(() => { observer.next("E") }, 5000); //emit this chunk of data later
+        setTimeout(() => { observer.complete() }, 6000); //send the completion status
+    });
+
+    array1 = [6, 7, 8, 9, 10];
+    array2 = ['F', 'G', 'H', 'I', 'J'];
+
+    //method 3
+    //will emit the arrays as is in each packet/chunk sent.
+    myObservable3 = of(this.array1, this.array2); //will emit the passed in data and automatically send the complete signal once complete...
+
+    //method 4
+    myObservable4 = from(this.array1); //will emit the passed in data and automatically send the complete signal once complete... arrays will be iterated and the values emitted seperately.
+
     ngOnInit(): void {
         //subscribe to the observable, log each chunk of data once it is
         this.myObservable.subscribe((val) => {
@@ -34,7 +57,31 @@ export class AppComponent implements OnInit {
         }, (error) => {
             alert(error.message);
         }, () => {
-            alert("Observable has completed emitting all values.");
-        })
+            alert("Observable1 has completed emitting all values.");
+        });
+
+        this.myObservable2.subscribe((val) => {
+            console.log(val);
+        }, (error) => {
+            alert(error.message);
+        }, () => {
+            alert("Observable2 has completed emitting all values.");
+        });
+
+        this.myObservable3.subscribe((val) => {
+            console.log(val);
+        }, (error) => {
+            alert(error.message);
+        }, () => {
+            alert("Observable3 has completed emitting all values.");
+        });
+
+        this.myObservable4.subscribe((val) => {
+            console.log(val);
+        }, (error) => {
+            alert(error.message);
+        }, () => {
+            alert("Observable4 has completed emitting all values.");
+        });
     }
 }
