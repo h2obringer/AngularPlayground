ca567f3d2c7e2958cedac16bbbeda71cc72a708d
h2obringer <randal.obringer@gmail.com>
2023-08-31 17:02:03 -0600

    Angular Routing - Baseline

--- AngularRouting/src/app/app.module.ts
+++ AngularRouting/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
