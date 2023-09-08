960222b15533527d81d42c739888af62c3cf4cdf
h2obringer <randal.obringer@gmail.com>
2023-09-08 09:58:49 -0600

    Reactive Form Example

--- AngularReactiveForms/src/app/app.component.ts
+++ AngularReactiveForms/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'AngularReactiveForms';
+
+    reactiveForm: FormGroup;
+
     country: string = '';
     defaultCountry = 'usa';
     firstName: string = '';
@@ -21,7 +25,20 @@ export class AppComponent {
         { id: 3, value: 'Other' }
     ];
 
+    ngOnInit(): void {
+        this.reactiveForm = new FormGroup({
+            firstName: new FormControl(null),
+            lastName: new FormControl(null),
+            email: new FormControl(null),
+            gender: new FormControl(this.defaultGender),
+            country: new FormControl(this.defaultCountry),
+            isSportsHobby: new FormControl(null),
+            isMoviesHobby: new FormControl(null),
+            isMusicHobby: new FormControl(null),
+        });
+    }
+
     onSubmit() {
-        // console.log(this.form);
+        console.log(this.reactiveForm);
     }
 }
