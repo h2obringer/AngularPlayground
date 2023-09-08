ddabcd90bb5d165dad7ba34a751a3690b339eac2
h2obringer <randal.obringer@gmail.com>
2023-09-08 09:22:30 -0600

    setValue and patchValue on Template Driven Forms - Example

--- AngularForms/src/app/app.component.ts
+++ AngularForms/src/app/app.component.ts
@@ -20,6 +20,32 @@ export class AppComponent {
     defaultGender: string = 'Male';
     @ViewChild('myForm') form: NgForm;
 
+    setDefaultValues() {
+        //NOTE: the structure of the object here must match the structure of the form.value section inside the ngForm object.
+        //this means all values must be set for the entire form here...
+        // this.form.setValue({
+        //     country: this.defaultCountry,
+        //     gender: this.defaultGender,
+        //     isSportsHobby: false,
+        //     isMoviesHobby: false,
+        //     isMusicHobby: false,
+        //     personalDetails: {
+        //         fname: 'John',
+        //         lname: 'Doe',
+        //         email: 'John.Doe@test.com'
+        //     }
+        // });
+
+        //patchValue allows us to update a specific value (or form group)
+        this.form.form.patchValue({
+            personDetails: {
+                fname: 'John',
+                lname: 'Doe',
+                email: 'John.Doe@test.com'
+            }
+        })
+    }
+
     onSubmit() {
         console.log(this.form);
     }
