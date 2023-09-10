f6896018125c01732420ae7e3ef9913a40aed618
h2obringer <randal.obringer@gmail.com>
2023-09-10 10:22:58 -0600

    Custom Async Validator - Reactive Forms - Example

--- AngularReactiveForms/src/app/app.component.ts
+++ AngularReactiveForms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -30,7 +31,7 @@ export class AppComponent implements OnInit {
             personalDetails: new FormGroup({
                 firstName: new FormControl(null, [Validators.required, this.noSpaceAllowed]),
                 lastName: new FormControl(null, [Validators.required, this.noSpaceAllowed]),
-                email: new FormControl(null, [Validators.required, Validators.email]),
+                email: new FormControl(null, [Validators.required, Validators.email], this.emailNotAllowed),
             }),
             gender: new FormControl(this.defaultGender),
             country: new FormControl(this.defaultCountry),
@@ -54,6 +55,21 @@ export class AppComponent implements OnInit {
             return { noSpaceAllowed: true }; //noSpaceAllowed is the name of our unique error code
         }
         return null;
+    }
+
+    //class: ng-pending will be added to the control while the async validator waits to finish.
+    emailNotAllowed(control: FormControl): Promise<any> | Observable<any> {
+        const response = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'procademy@gmail.com') {
+                    resolve({ emailNotAllowed: true });
+                } else {
+                    resolve(null);
+                }
+            }, 5000);
+        });
+
+        return response;
     }
 
     onSubmit() {
